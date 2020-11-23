2fb9feef96446a530d420c1d0a1a963fb7091cda
0199kye <70757090+0199kye@users.noreply.github.com>
2020-11-23 22:52:11 +0900

    trial 2

--- main.c
+++ main.c
@@ -8,14 +8,24 @@
 	
 int main(int argc, char *argv[]) {
 	
-	int gameboard [N][N];
+	int gameboard [N][N]= {
+	{0,0,0,0,0,0},
+	{0,0,0,0,0,0},
+	{0,0,1,2,0,0},
+	{0,0,2,1,0,0},
+	{0,0,0,0,0,0},
+	{0,0,0,0,0,0}
+	};
+	
 	int x,y;
+	int i,j;
 	int turn = 1;
 	int white = 1;
 	int black = 2; 
 	int flip;
 	int flip number;
-	
+	int isGameEnd; //게임 종료 조건에 관한 거 코딩하기
+	 
 	int east = gameboard [x+1][y];
 	int west = gameboard [x-1][y];
 	int north = gameboard [x][y+1];
@@ -25,16 +35,22 @@ int main(int argc, char *argv[]) {
 	int southeast = gameboard [x+1][y-1];
 	int southwest = gameboard [x-1][y-1]; //방향에 대한 정의  
 	
-	//그 가운데 4개 배열에다가 초기값으로 입력 넣어야함.
+	
+	
+	printf("white is number1, black is number2\n"); //검은색 하얀색을 어떻게 나타내야할지 몰라서 숫자로 대신함. 
+	printf("\n");
 	 
-    for(i=0; i<N, i++)
+    for(i=0; i<N; i++)
     {
     	for(j=0; j<N; j++)
     	 printf("%d", gameboard[i][j]);
     	printf("\n");
 	} //게임판 출력  
 	 
+	//game 종료 조건 적 기  
+	
 	while(1)
+	
 	{
 	  if (turn==1)  //하얀색 돌을 놓을 차례 
 	  
@@ -42,17 +58,13 @@ int main(int argc, char *argv[]) {
     
     /*flip이 불가능한 경우*/
 	 
-    /*flip이 가능한 경우*/
+    /*flip이 가능한 경우, 먼저 현 상태 출력해야함*/ 
 	 
 	    {
 		printf("put new white\n");
 	    scanf("%d %d", &gameboard[x][y]);
 	    }
 	    
-	    if (gameboard[x][y] !=0)
-	    {
-	    	printf("You have placed othello on wrong spot. Try again. (already occupied) \n"); //다른 오델로가 이미 있는 곳에 오델로를 놓을려고 한 경우, 다시하세요. 
-		}
 		
 		 
 		
@@ -78,10 +90,13 @@ int main(int argc, char *argv[]) {
 		 
 		turn --;
 		
-    	
-       //돌을 다 놓아서 더 이상 놓을 자리가 없다면 게임 끝내야함 
+    	else (gameboard[x][y] !=0)
+	    {
+	    	printf("You have placed othello on wrong spot. Try again. (already occupied) \n"); //다른 오델로가 이미 있는 곳에 오델로를 놓을려고 한 경우, 다시하세요. 
+		}
+      
        //돌의 개수를 세서 누가 더 많은지로 우승자 가리기 
-	
+	     
 	
 	
 	return 0;
