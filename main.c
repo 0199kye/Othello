0ac35ad905ae5ef983c8cd3f43b498e3351f2cf0
0199kye <70757090+0199kye@users.noreply.github.com>
2020-11-24 22:18:36 +0900

    trial3

--- main.c
+++ main.c
@@ -23,29 +23,48 @@ int main(int argc, char *argv[]) {
 	int white = 1;
 	int black = 2; 
 	int flip;
-	int flip number;
-	int isGameEnd; //게임 종료 조건에 관한 거 코딩하기
+	int flip_number;
 	 
-	int east = gameboard [x+1][y];
-	int west = gameboard [x-1][y];
-	int north = gameboard [x][y+1];
-	int south = gameboard [x][y-1];
-	int northeast = gameboard [x+1][y+1];
-	int northwest = gameboard [x-1][y+1];
-	int southeast = gameboard [x+1][y-1];
-	int southwest = gameboard [x-1][y-1]; //방향에 대한 정의  
+	int east = gameboard [y][x+1];
+	int west = gameboard [y][x-1];
+	int north = gameboard [y-1][x];
+	int south = gameboard [y+1][x];
+	int northeast = gameboard [y-1][x+1];
+	int northwest = gameboard [y-1][x-1];
+	int southeast = gameboard [y+1][x+1];
+	int southwest = gameboard [y+1][x-1]; //방향에 대한 정의  
 	
+	int flipnum_east;
+	int flipnum_west;
+	int flipnum_north;
+	int flipnum_south;
+	int flipnum_northeast;
+	int flipnum_northwest;
+	int flipnum_southeast;
+	int flipnum_southwest;
 	
+	int total_black;
+	int total_white;
 	
-	printf("white is number1, black is number2\n"); //검은색 하얀색을 어떻게 나타내야할지 몰라서 숫자로 대신함. 
+	printf("white is number1, black is number2, empty space is 0\n");//검은색 하얀색을 숫자로 대신함. 
+	printf("Refer to the following coordinate of gameboard.\n"); 
 	printf("\n");
-	 
+	printf("[0][0] [0][1] [0][2] [0][3] [0][4] [0][5]\n");
+	printf("[1][0] [1][1] [1][2] [1][3] [1][4] [1][5]\n");
+	printf("[2][0] [2][1] [2][2] [2][3] [2][4] [2][5]\n");
+	printf("[3][0] [3][1] [3][2] [3][3] [3][4] [3][5]\n");
+	printf("[4][0] [4][1] [4][2] [4][3] [4][4] [4][5]\n");
+	printf("[5][0] [5][1] [5][2] [5][3] [5][4] [5][5]\n"); //gameboard 판 좌표 (othello위치 입력할 때 헷갈림 방지용) 
+	printf("\n");
+	printf("\n");
+	
+	printf("This is initial state.\n"); 
     for(i=0; i<N; i++)
     {
     	for(j=0; j<N; j++)
     	 printf("%d", gameboard[i][j]);
     	printf("\n");
-	} //게임판 출력  
+	} //게임판 초기 상태 출력  
 	 
 	//game 종료 조건 적 기  
 	
@@ -54,50 +73,112 @@ int main(int argc, char *argv[]) {
 	{
 	  if (turn==1)  //하얀색 돌을 놓을 차례 
 	  
-    //flip이 가능한지 여부 먼저 파악
+	    // for문을 돌려서 게임보드에 0(빈 공간)이 있는 지 먼저 확인 없으면 break걸 기 
+	    
+       //flip이 가능한지 여부 먼저 파악 
     
-    /*flip이 불가능한 경우*/
+    /*flip이 불가능한 경우 turn을 2로 바꿈 */
+    printf("There is no space available for new white othello.\n");
+    printf("So the turn goes to black player.\n");
+    turn=2; break;
 	 
-    /*flip이 가능한 경우, 먼저 현 상태 출력해야함*/ 
-	 
+    /*flip이 가능한 경우*/ 
+        printf("This is current state.\n");
+		 
+         for(i=0; i<N; i++)
+         {
+    	   for(j=0; j<N; j++)
+    	     printf("%d", gameboard[i][j]);
+    	   printf("\n");
+	     } //게임판 출력  
+	
 	    {
-		printf("put new white\n");
-	    scanf("%d %d", &gameboard[x][y]);
+		printf("Put new white(1) othello.\n");
+	    scanf("%d %d", gameboard[y][x]);
 	    }
 	    
-		
+		else (gameboard[y][x] !=0)
+	    {
+	    	printf("You have placed othello on wrong spot. Try again. (already occupied) \n"); //다른 오델로가 이미 있는 곳에 오델로를 놓을려고 한 경우, 다시하세요. 
+		}
 		 
 		
 		/*플레이어가 입력한 곳이flip 이 불가능하다면, 다시 시대해보세요 출력*/
 		
 		/*플레이어가 입력한 곳이 flip이 가능하다면, flip 하고 그 횟수 출력*/
-		 
-		flip number++;
-		 
-		printf("you have flipped %d othellos\n", flip number); 
+		
+		flip_number = flipnum_east + flipnum_west + flipnum_north + flipnum_south + flipnum_northeast + flipnum_northwest + flipnum_southeast + flipnum_southwest;
+		
+		printf("Result of this turn.\n");
+		
+		for(i=0; i<N; i++)
+         {
+    	   for(j=0; j<N; j++)
+    	     printf("%d", gameboard[i][j]);
+    	   printf("\n");
+	     }
+	     
+		printf("East: %d, West: %d, North: %d, South: %d, Northeast: %d, Northwest: %d, Southeast: %d, Southwest: %d\n", 
+		 flipnum_east, flipnum_west, flipnum_north, flipnum_south, flipnum_northeast, flipnum_northwest, flipnum_southeast, flipnum_southwest);
+		
+		printf("you have flipped %d othellos\n", flip_number); 
 		 
 	    turn ++;
 	    
-    	}
-	
-	
-	  if (turn== 2)  //검은색 돌을 놓을 차례 
+	    
+	     if (turn== 2)  //검은색 돌을 놓을 차례 
     	{
-		printf("put new black\n");
-		scanf("%d %d", &gameboard[x][y]);
+		printf("put new black(2)\n");
+		scanf("%d %d", gameboard[y][x]);
 	    }
 		//위에 하얀색 코딩한 거 복사 붙여넣기 
 		 
 		turn --;
 		
-    	else (gameboard[x][y] !=0)
-	    {
-	    	printf("You have placed othello on wrong spot. Try again. (already occupied) \n"); //다른 오델로가 이미 있는 곳에 오델로를 놓을려고 한 경우, 다시하세요. 
-		}
-      
-       //돌의 개수를 세서 누가 더 많은지로 우승자 가리기 
-	     
+		
+    	}
 	
+	
+	/*이제 누가 이겼는지 결과 확인*/ 
+    for(i=0;i<N;i++)
+    {
+    	for(j=0;j<N;j++)
+    	
+    	if(gameboard [i][j] ==1)
+    	{
+    		total_white++;
+		}
+	}
+	
+	printf("Number of White Othello is %d\n", total_white);
+	 
+	 
+	 for(i=0;i<N;i++)
+    {
+    	for(j=0;j<N;j++)
+    	
+    	if(gameboard [i][j] ==2)
+    	{
+    		total_black++;
+		}
+	} //따로 실행해본 결과 검은색 개수가 이상하게 나옴. 다시 확인해볼 것 
+		
+    printf("Number of Black Othello is %d\n", total_black);
+		
+    if(total_white > total_black)
+	{
+		printf("White win\n");
+	}  
+	
+    else if(total_black > total_white)
+	{
+		printf("Black win\n");
+	}
+      
+    else 
+	{
+		printf("There is no winner or loser\n");
+	}
 	
 	return 0;
 }
